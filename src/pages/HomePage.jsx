317869dbf18ac4b8194ed95fc53f7367104943b2
sachin kunwar <sachinkunwar284@gmail.com>
2025-05-21 15:48:41 +0545

    fetch books api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,32 @@
-import Button from "../components/Button"
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
+import axios from 'axios'
 
 function HomePage(){
+  const [books,setBooks] = useState([])
+  // let books = []
+  const fetchBooks  = async ()=>{
+   const response = await axios.get("http://localhost:4000/api/books/") // call api 
+   setBooks(response.data.datas) // store coming response on above books state
+  //  books.push(response.data.datas)
+  }
+   useEffect(()=>{
+    fetchBooks()
+   },[])
+
+
     return(
        <>
       <Navbar />
       <div className="flex flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          books.map(function(book){ // books vanne state lai loop garyo using map higher order function, arrayToLoop.map(function(arrayKoEachItem){ return ()})
+            return (
+              <Card book={book} key={book.id} />
+            )
+          })
+        }
       </div>
        </>
     )
