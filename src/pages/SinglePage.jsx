8e759f5af36a20da9823744537cf3715e49e0087
sachin kunwar <sachinkunwar284@gmail.com>
2025-05-24 07:42:55 +0545

    edit book page

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,11 +1,42 @@
+
+import {  Link, useNavigate, useParams } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Button from "../components/Button"
 
 
 function SinglePage(){
+   const navigate =  useNavigate()
+    const {id} = useParams() // used to get the parameter of route ..
+    const [book,setBook] = useState({})
+    const fetchBook = async ()=>{
+       const response = await axios.get("http://localhost:4000/api/books/" + id)
+       setBook(response.data.datas)// response.data.hello
+    }
+    useEffect(()=>{
+        fetchBook()
+    },[])
+
+    const deleteBook = async ()=>{
+        const response = await axios.delete("http://localhost:4000/api/books/" + id)
+        if(response.status === 200){
+            // home page ma navigation gardim
+            navigate("/")
+        }else{
+            alert("Something went wrong")
+        }
+        // console.log("Delete trigered vayo hai function ....")
+    }
     return(
         <>
        <Navbar />
-        <h1>this is single page</h1>
+        <h1>{book.bookName}</h1>
+        <p>{book.price}</p>
+        <p>{book.bookAuthor}</p>
+        <button onClick={deleteBook}>Delete Me</button>
+        <Link to={`/edit-page/${book.id}`}><button>Edit Me</button></Link>
+        
         </>
     )
 }
